08b867062b8444475b150ca7cee325b874c8a32f
Shivam Verma <shivamgverma99@gmail.com>
2025-06-25 10:00:19 +0530

    Add files via upload

---
+++ countDigit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(int n) {
+    int cnt = 0;
+    while (n > 0) {
+        cnt = cnt + 1;
+        n = n / 10;
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "Total Digit: " <<count(n) << endl; 
+    return 0;
+}
